aab77d9bd4887fd31529dd7cd05859834f421560
Heeyoung Song <hsong8179@gmail.com>
2020-09-29 17:59:00 -0400

    update

---
+++ exercise101/read_and_write.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const { cpuUsage } = require("process");
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+readline.question("Input file: ", function(inputFile){
+    fs.readFile(inputFile, function(error, buffer){
+        if(error){
+            console.log(error.message);
+            readline.close();
+            return;
+        }
+        readline.question("Output file: ", function(outputFile){
+            readline.close();
+            const content = buffer.toString();
+            const upperCased = content.toUpperCase();
+            fs.writeFile(outputFile, upperCased, function(erro){
+                if(error){
+                    console.log(error.message);
+                    return;
+                }
+                console.log("Wrote to file" , outputFile)
+            })
+        })
+    })
+})
